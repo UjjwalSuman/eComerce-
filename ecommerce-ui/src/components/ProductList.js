1d8bf7f64b164f0c51bf992c3c89191b1576bacb
Ujjwal Suman <ujjwalsuman007@gmail.com>
2026-01-29 21:18:43 +0530

    Fix Naming

--- ecommerce-ui/src/components/ProductList.js
+++ ecommerce-ui/src/components/ProductList.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import ProductCard from "./productCard";
+import ProductCard from "./ProductCard";
 import SearchBar from "./SearchBar";
 import SortDropdown from "./SortDropdown";
 
